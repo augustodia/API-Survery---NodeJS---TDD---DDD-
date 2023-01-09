3c5e451c238f775fc74cd4f4b9ce0eee3e112bdf
Luiz Augusto <augustodia97@gmail.com>
2023-01-09 20:54:50 -0300

    feat: :sparkles: ensure SignUpController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -6,7 +6,12 @@ export class SignUpController {
   constructor() {}
 
   handle(httpRequest: HttpRequest): HttpResponse {
-    const requiredFields = ["name", "email", "password"];
+    const requiredFields = [
+      "name",
+      "email",
+      "password",
+      "passwordConfirmation",
+    ];
 
     for (const field of requiredFields) {
       if (!httpRequest.body[field]) {
