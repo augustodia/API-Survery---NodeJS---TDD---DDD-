2694d76d89c2d241efd9eda25b4d9efa89c207a3
Luiz Augusto <augustodia97@gmail.com>
2023-01-08 22:29:26 -0300

    feat: :sparkles: ensure SignUpController return 400 if no email is provided

--- src/presentation/controllers/SignUp.ts
+++ src/presentation/controllers/SignUp.ts
@@ -2,9 +2,17 @@ export class SignUpController {
   constructor() {}
 
   handle(httpRequest: any): any {
-    return {
-      statusCode: 400,
-      body: new Error("Missing param: name"),
-    };
+    if (!httpRequest.body.name) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: name"),
+      };
+    }
+    if (!httpRequest.body.email) {
+      return {
+        statusCode: 400,
+        body: new Error("Missing param: email"),
+      };
+    }
   }
 }
