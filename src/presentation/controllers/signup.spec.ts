723da7d3b645cc1f606af7533646a93af91ae12b
Luiz Augusto <augustodia97@gmail.com>
2023-01-09 20:55:00 -0300

    test: :white_check_mark: ensure SignUpController returns 400 if no passwordConfirmation is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -48,4 +48,22 @@ describe("SignUp Controller", () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new Error("Missing param: password"));
   });
+
+  it("Should return 400 if no passwordConfirmation is provided", async () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+      },
+    };
+
+    const httpResponse = sut.handle(httpRequest);
+
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(
+      new Error("Missing param: passwordConfirmation")
+    );
+  });
 });
